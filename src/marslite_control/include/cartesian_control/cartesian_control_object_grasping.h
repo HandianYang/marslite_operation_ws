20891e2358588b9bd83f1c5193a8e07050541348
HandianYang <ych0610765@gmail.com>
2025-07-16 17:53:23 +0800

    Modify detected_object msg

--- src/marslite_control/include/cartesian_control/cartesian_control_object_grasping.h
+++ src/marslite_control/include/cartesian_control/cartesian_control_object_grasping.h
@@ -9,21 +9,24 @@
 #include "detection_msgs/DetectedObjectArray.h"
 #include "detection_msgs/DriveToInitialPoint.h"
 #include "detection_msgs/DriveToObservePoint.h"
-#include "detection_msgs/TriggerRecording.h"
-#include "detection_msgs/ObjectGrasping.h"
+#include "detection_msgs/GraspObjectWithID.h"
+#include "detection_msgs/RecordObjectPosition.h"
+
 
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
 
-const static int kFrameToRecord = 10;
+// const static int kFrameToRecord = 10;
 
+// base_frame: /base_link
+// end_effector_frame: /tm_gripper
 const geometry_msgs::PoseStamped kInitialGripperPose = [] {
   geometry_msgs::PoseStamped pose_stamped;
   pose_stamped.header.frame_id = "base_link";
-  pose_stamped.pose.position.x = 0.712;
+  pose_stamped.pose.position.x = 0.888;
   pose_stamped.pose.position.y = -0.122;
-  pose_stamped.pose.position.z = 1.043;
+  pose_stamped.pose.position.z = 1.035;
   pose_stamped.pose.orientation.x = 0.5;
   pose_stamped.pose.orientation.y = 0.5;
   pose_stamped.pose.orientation.z = 0.5;
@@ -31,23 +34,26 @@ const geometry_msgs::PoseStamped kInitialGripperPose = [] {
   return pose_stamped;
 }();
 
+// observing point for shelves
+//   base_frame: /base_link
+//   end_effector_frame: /tm_gripper
 const geometry_msgs::PoseStamped kObservingGripperPose = [] {
   geometry_msgs::PoseStamped pose_stamped;
   pose_stamped.header.frame_id = "base_link";
-  pose_stamped.pose.position.x = 0.621;
-  pose_stamped.pose.position.y = -0.129;
-  pose_stamped.pose.position.z = 0.912;
-  pose_stamped.pose.orientation.x = 0.616;
-  pose_stamped.pose.orientation.y = 0.583;
-  pose_stamped.pose.orientation.z = 0.369;
-  pose_stamped.pose.orientation.w = 0.380;
+  pose_stamped.pose.position.x = 0.872;
+  pose_stamped.pose.position.y = -0.123;
+  pose_stamped.pose.position.z = 0.969;
+  pose_stamped.pose.orientation.x = 0.578;
+  pose_stamped.pose.orientation.y = 0.577;
+  pose_stamped.pose.orientation.z = 0.408;
+  pose_stamped.pose.orientation.w = 0.408;
   return pose_stamped;
 }();
 
 class CartesianControlObjectGrasping {
 public:
   explicit CartesianControlObjectGrasping(const ros::NodeHandle& nh = ros::NodeHandle());
-  void run();
+  // void run();
 
 private:
   ros::NodeHandle nh_;
@@ -55,33 +61,84 @@ private:
   ros::Publisher target_frame_pub_;
   ros::Publisher gripper_pub_;
   ros::Subscriber detected_objects_sub_;
+  // TODO: Move out to test
   ros::ServiceServer drive_to_initial_point_service_;
   ros::ServiceServer drive_to_observe_point_service_;
-  ros::ServiceServer trigger_recording_service_;
-  ros::ServiceServer object_grasping_service_;
+  ros::ServiceServer record_object_position_service_;
+  ros::ServiceServer grasp_object_with_id_service_;
 
   detection_msgs::DetectedObjectArray detected_objects_;
+  detection_msgs::DetectedObjectArray recorded_objects_;
   geometry_msgs::PoseStamped target_gripper_pose_;
   std_msgs::Bool gripper_;
 
-  std::unordered_map<int, std::vector<geometry_msgs::Point>> record_buffer_;
-  std::unordered_map<int, geometry_msgs::Point> recorded_object_positions_;
-
-  // flags
-  bool has_pending_record_;
-
 private:
   // service trigger functions
-  bool driveToInitialPoint(detection_msgs::DriveToInitialPoint::Request& req,
-                           detection_msgs::DriveToInitialPoint::Response& res);
-  bool driveToObservePoint(detection_msgs::DriveToObservePoint::Request& req,
-                           detection_msgs::DriveToObservePoint::Response& res);
-  bool recordInitialPosition(detection_msgs::TriggerRecording::Request& req,
-                             detection_msgs::TriggerRecording::Response& res);
-  bool graspObjectWithId(detection_msgs::ObjectGrasping::Request& req,
-                         detection_msgs::ObjectGrasping::Response& res);
+  // TODO: Move out to test
+  inline bool driveToInitialPoint(
+      detection_msgs::DriveToInitialPoint::Request& req,
+      detection_msgs::DriveToInitialPoint::Response& res)
+  {
+    target_frame_pub_.publish(kInitialGripperPose);
+    return true;
+  }
 
-  void detectedObjectsCallback(const detection_msgs::DetectedObjectArray::ConstPtr& msg);
+  inline bool driveToObservePoint(
+      detection_msgs::DriveToObservePoint::Request& req,
+      detection_msgs::DriveToObservePoint::Response& res)
+  {
+    target_frame_pub_.publish(kObservingGripperPose);
+    return true;
+  }
+  
+  inline bool recordObjectPosition(
+      detection_msgs::RecordObjectPosition::Request& req,
+      detection_msgs::RecordObjectPosition::Response& res)
+  {
+    recorded_objects_ = detected_objects_;
+    if (recorded_objects_.objects.empty()) {
+      res.result = "No objects detected to record.";
+      res.success = false;
+      return false;
+    }
+
+    std::ostringstream oss;
+    oss << recorded_objects_.objects.size() << " objects detected.\n";
+    for (const auto& object : recorded_objects_.objects) {
+      oss << "[" << std::fixed << std::setprecision(3)
+          << object.centroid.x << ", "
+          << object.centroid.y << ", "
+          << object.centroid.z << "]\n";
+    }
+    res.result = oss.str();
+    res.success = true;
+    return true;
+  }
+            
+  inline bool graspObjectWithId(
+      detection_msgs::GraspObjectWithID::Request& req,
+      detection_msgs::GraspObjectWithID::Response& res)
+  {
+    if (req.id < 0 || req.id >= static_cast<int>(recorded_objects_.objects.size())) {
+      res.result = "Invalid object ID! Skip grasping.";
+      res.success = false;
+      return false;
+    }
+
+    target_gripper_pose_.header.frame_id = "base_link";
+    target_gripper_pose_.pose.position = recorded_objects_.objects[req.id].centroid;
+    target_gripper_pose_.pose.orientation = kInitialGripperPose.pose.orientation;
+    target_frame_pub_.publish(target_gripper_pose_);
+    res.result = "Grasping object with ID " + std::to_string(req.id);
+    res.success = true;
+    return true;
+  }
+
+  // callbacks
+  inline void detectedObjectsCallback(const detection_msgs::DetectedObjectArray::ConstPtr& msg)
+  {
+    detected_objects_ = *msg;
+  }
 };
 
 #endif // #ifndef MARSLITE_CONTROL_CARTESIAN_CONTROL_OBJECT_GRASPING_H
--- src/marslite_control/src/cartesian_control_object_grasping.cpp
+++ src/marslite_control/src/cartesian_control_object_grasping.cpp
@@ -5,7 +5,7 @@
  ****************************************************** */
 
 CartesianControlObjectGrasping::CartesianControlObjectGrasping(const ros::NodeHandle& nh)
-    : nh_(nh), rate_(ros::Rate(25)), has_pending_record_(false)
+    : nh_(nh), rate_(ros::Rate(25))
 {
   target_frame_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/target_frame", 10);
   gripper_pub_ = nh_.advertise<std_msgs::Bool>("/gripper/cmd_gripper", 1);
@@ -15,102 +15,8 @@ CartesianControlObjectGrasping::CartesianControlObjectGrasping(const ros::NodeHa
       "/drive_to_initial_point", &CartesianControlObjectGrasping::driveToInitialPoint, this);
   drive_to_observe_point_service_ = nh_.advertiseService(
       "/drive_to_observe_point", &CartesianControlObjectGrasping::driveToObservePoint, this);
-  trigger_recording_service_ = nh_.advertiseService(
-      "/record_initial_position", &CartesianControlObjectGrasping::recordInitialPosition, this);
-  object_grasping_service_ = nh_.advertiseService(
+  record_object_position_service_ = nh_.advertiseService(
+      "/record_object_position", &CartesianControlObjectGrasping::recordObjectPosition, this);
+  grasp_object_with_id_service_ = nh_.advertiseService(
       "/grasp_object_with_id", &CartesianControlObjectGrasping::graspObjectWithId, this);
-}
-
-/******************************************************
- *                  Public members                    *  
- ****************************************************** */
-
-void CartesianControlObjectGrasping::run()
-{
-  while (ros::ok()) {
-    
-
-    rate_.sleep();
-    ros::spinOnce();
-  }
-}
-
-/******************************************************
- *                  Private members                   *  
- ****************************************************** */
-
-bool CartesianControlObjectGrasping::driveToInitialPoint(
-    detection_msgs::DriveToInitialPoint::Request& req,
-    detection_msgs::DriveToInitialPoint::Response& res)
-{
-  target_frame_pub_.publish(kInitialGripperPose);
-  return true;
-}
-
-bool CartesianControlObjectGrasping::driveToObservePoint(
-    detection_msgs::DriveToObservePoint::Request& req,
-    detection_msgs::DriveToObservePoint::Response& res)
-{
-  target_frame_pub_.publish(kObservingGripperPose);
-  return true;
-}
-
-bool CartesianControlObjectGrasping::recordInitialPosition(
-    detection_msgs::TriggerRecording::Request& req,
-    detection_msgs::TriggerRecording::Response& res)
-{
-  ROS_INFO(" Recording initial positions started...");
-  record_buffer_.clear();
-  has_pending_record_ = true;
-  res.success = true;
-  return true;
-}
-
-bool CartesianControlObjectGrasping::graspObjectWithId(
-    detection_msgs::ObjectGrasping::Request& req,
-    detection_msgs::ObjectGrasping::Response& res)
-{
-  ROS_INFO("  Grasping object with id=%d...", req.id);
-  target_gripper_pose_.header.frame_id = "base_link";
-  target_gripper_pose_.pose.position = recorded_object_positions_[req.id];
-  target_gripper_pose_.pose.orientation = kInitialGripperPose.pose.orientation;
-  target_frame_pub_.publish(target_gripper_pose_);
-  res.success = true;
-  return true;
-}
-
-void CartesianControlObjectGrasping::detectedObjectsCallback(
-    const detection_msgs::DetectedObjectArray::ConstPtr& msg)
-{
-  detected_objects_ = *msg;
-
-  if (has_pending_record_) {
-    for (const detection_msgs::DetectedObject& object : detected_objects_.objects) {
-      if (record_buffer_.find(object.id) == record_buffer_.end()) {
-        record_buffer_[object.id] = std::vector<geometry_msgs::Point>();
-      }
-      record_buffer_[object.id].push_back(object.position);
-    }
-
-    // Check if any object appears in more than 5 frames
-    bool any_object_has_enough_frames = false;
-    for (const auto& [id, positions] : record_buffer_) {
-      if (positions.size() > kFrameToRecord) {
-        any_object_has_enough_frames = true;
-        break;
-      }
-    }
-
-    if (any_object_has_enough_frames) {
-      has_pending_record_ = false;
-      for (const auto& [id, positions] : record_buffer_) {
-        if (positions.size() < 5) {
-          // ROS_WARN("Not enough positions recorded for object ID %d. Expected %d, got %zu.", id, kFrameToRecord, positions.size());
-          continue;
-        }
-        recorded_object_positions_[id] = positions.back(); // Get the last recorded position
-      }
-      ROS_INFO(" Recording initial positions completed.");
-    }
-  }
 }
